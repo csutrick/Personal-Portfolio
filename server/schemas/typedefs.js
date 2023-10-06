64365bf02a0e10315634492de9bf5922e7d5ab77
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2023-10-06 00:16:02 -0500

    Added basic schema

--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -1,7 +1,17 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  
+    type Offer {
+        _id: ID
+        OfferText: String
+        offerSender: String
+        createdAt: String
+    }
+
+    type Query {
+        offers: [Offer]!
+        offer(offerId: ID!): Offer
+    }
 `;
 
 module.exports = typeDefs;
