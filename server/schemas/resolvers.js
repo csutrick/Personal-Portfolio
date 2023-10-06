e37e443fb5c37d39a77512fb5962ae18772cef7f
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2023-10-06 12:54:22 -0500

    Added test mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -9,7 +9,17 @@ const resolvers = {
         offer: async (parent, { offerId }) => {
             return Offer.findOne({ _id: offerId })
         },
-    }
+    },
+
+    Mutation: {
+        addOffer: async (parent, { offerText, offerSender }) => {
+            return Offer.create({ offerText, offerSender });
+        },
+
+        removeOffer: async (parent, { offerId }) => {
+            return Offer.findOneAndDelete({ _id: offerId })
+        },
+    },
 };
 
 module.exports = resolvers;
