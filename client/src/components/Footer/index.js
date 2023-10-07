100d041b4bf42d5cf21ef099db84a398860b526c
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2023-10-07 18:11:23 -0500

    Basic footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className='bg-gray-400 text-center'>
-            <h1 className='text-black'>Footer</h1>
+        <footer className='flex w-full bg-[#84D2F6] justify-center'>
+            <div className='mx-12 flex justify-center items-center w-[1250px] h-[100px] mb-16
+            bg-white rounded-xl'>
+                <h3 className='font-bold'>Footer</h3>
+            </div>
         </footer>
     );
 };
