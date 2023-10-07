f5dea8af06ac5594d4e3d5a3dec9a077354ea0bb
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2023-10-07 13:01:47 -0500

    Added mutation, everything is hooked up and working

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,7 +5,6 @@ import OfferForm from '../components/OfferForm';
 
 
 const Home = () => {
-
     return (
        <main className='bg-red-400 text-center border-black border-b-2 border-t-2'>
             <OfferForm />
