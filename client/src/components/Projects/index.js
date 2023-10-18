787012ee6861dcdb38d940ea63fcb5ea52906834
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2023-10-18 13:11:00 -0500

    Redid projects section, redid layout

--- client/src/components/Projects/index.js
+++ client/src/components/Projects/index.js
@@ -1,49 +1,76 @@
 import React from "react";
+import { AiFillGithub } from 'react-icons/ai'
+import pic from '../../Assets/project-thumbnails/Untitled-1.png';
 
 const Projects = () => {
     return (
         <section className='w-full flex justify-center py-16'>
-            <div className='mx-12 w-[1250px] flex flex-col md:grid grid-cols-6 grid-rows-3 gap-5'>
-                {/* Project 1 */}
-                <div className='bg-gray-200 p-4 col-start-1 col-end-5 relative drop-shadow-md hover:drop-shadow-2xl
-                group rounded-xl border-[#84D2F6] hover:border-[#59A5D8] border-8 h-[300px] hover:scale-[1.02] transition-all duration-200 ease-in-out'>
-                    <div className='absolute bottom-5 -left-1 p-2 pl-3 bg-[#84D2F6] group-hover:bg-[#59A5D8] transition-colors duration-200 ease-in-out rounded-r-lg max-w-[250px]'>
+            <div className='mx-12 w-[1250px] flex flex-col md:flex-row items-center justify-between flex-wrap'>
+                {/* Hero Project */}
+                <a href="http://example.com/" target="_blank" rel="noopener noreferrer"
+                className='group relative flex h-[300px] w-[100%] mb-12 transition-all duration-300 ease-in-out
+                rounded-xl drop-shadow-md hover:drop-shadow-2xl border-8 border-[#84D2F6] hover:border-[#59A5D8] hover:scale-[1.02]
+                bg-center bg-cover bg-no-repeat backdrop-blur-none hover:backdrop-blur-sm' style={{ backgroundImage: `url(${pic})` }}>
+                    <div className='absolute bottom-5 -left-1 p-2 pl-3 bg-[#84D2F6] group-hover:bg-[#59A5D8] transition-colors duration-300 ease-in-out rounded-r-lg max-w-[250px]'>
                         <h2 className='font-bold text-white text-lg'>Project 1</h2>
                         <h3 className='text-sm text-[#133C55]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, sint?</h3>
                     </div>
-                </div>
+                    <a href="https://github.com/csutrick" className='absolute bottom-0 right-0 p-2 '>
+                        <AiFillGithub className='text-5xl hover:scale-[115%] text-[#84D2F6] group-hover:text-[#6bce70] transition-all duration-300 ease-in-out'/>
+                    </a>
+                </a>
                 {/* Project 2 */}
-                <div className='bg-gray-200 p-4 col-start-5 col-end-7 row-start-1 row-end-3 relative drop-shadow-md hover:drop-shadow-2xl
-                group rounded-xl border-[#84D2F6] hover:border-[#59A5D8] border-8 h-[300px] md:h-auto hover:scale-[1.02] transition-all duration-200 ease-in-out'>
-                    <div className='absolute bottom-5 -left-1 p-2 pl-3 bg-[#84D2F6] group-hover:bg-[#59A5D8] transition-colors duration-200 ease-in-out rounded-r-lg max-w-[250px]'>
-                        <h2 className='font-bold text-white text-lg'>Project 2</h2>
+                <a href="http://example.com/" target="_blank" rel="noopener noreferrer"
+                className='group relative flex h-[300px] w-[100%] md:w-[48%] mb-12 transition-all duration-300 ease-in-out
+                rounded-xl drop-shadow-md hover:drop-shadow-2xl border-8 border-[#84D2F6] hover:border-[#59A5D8] hover:scale-[1.02]
+                bg-center bg-cover bg-no-repeat backdrop-blur-none hover:backdrop-blur-sm' style={{ backgroundImage: `url(${pic})` }}>
+                    <div className='absolute bottom-5 -left-1 p-2 pl-3 bg-[#84D2F6] group-hover:bg-[#59A5D8] transition-colors duration-300 ease-in-out rounded-r-lg max-w-[250px]'>
+                        <h2 className='font-bold text-white text-lg'>Project 1</h2>
                         <h3 className='text-sm text-[#133C55]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, sint?</h3>
                     </div>
-                </div>
+                    <a href="https://github.com/csutrick" className='absolute bottom-0 right-0 p-2 '>
+                        <AiFillGithub className='text-5xl hover:scale-[115%] text-[#84D2F6] group-hover:text-[#6bce70] transition-all duration-300 ease-in-out'/>
+                    </a>
+                </a>
                 {/* Project 3 */}
-                <div className='bg-gray-200 p-4 col-start-3 col-end-5 row-start-2 row-end-3 relative drop-shadow-md hover:drop-shadow-2xl
-                group rounded-xl border-[#84D2F6] hover:border-[#59A5D8] border-8 h-[300px] hover:scale-[1.02] transition-all duration-200 ease-in-out'>
-                    <div className='absolute bottom-5 -left-1 p-2 pl-3 bg-[#84D2F6] group-hover:bg-[#59A5D8] transition-colors duration-200 ease-in-out rounded-r-lg max-w-[250px]'>
-                        <h2 className='font-bold text-white text-lg'>Project 3</h2>
+                <a href="http://example.com/" target="_blank" rel="noopener noreferrer"
+                className='group relative flex h-[300px] w-[100%] md:w-[48%] mb-12 transition-all duration-300 ease-in-out
+                rounded-xl drop-shadow-md hover:drop-shadow-2xl border-8 border-[#84D2F6] hover:border-[#59A5D8] hover:scale-[1.02]
+                bg-center bg-cover bg-no-repeat backdrop-blur-none hover:backdrop-blur-sm' style={{ backgroundImage: `url(${pic})` }}>
+                    <div className='absolute bottom-5 -left-1 p-2 pl-3 bg-[#84D2F6] group-hover:bg-[#59A5D8] transition-colors duration-300 ease-in-out rounded-r-lg max-w-[250px]'>
+                        <h2 className='font-bold text-white text-lg'>Project 1</h2>
                         <h3 className='text-sm text-[#133C55]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, sint?</h3>
                     </div>
-                </div>
+                    <a href="https://github.com/csutrick" className='absolute bottom-0 right-0 p-2 '>
+                        <AiFillGithub className='text-5xl hover:scale-[115%] text-[#84D2F6] group-hover:text-[#6bce70] transition-all duration-300 ease-in-out'/>
+                    </a>
+                </a>
                 {/* Project 4 */}
-                <div className='bg-gray-200 p-4 col-start-1 col-end-3 row-start-2 row-end-4 relative drop-shadow-md hover:drop-shadow-2xl
-                group rounded-xl border-[#84D2F6] hover:border-[#59A5D8] border-8 h-[300px] md:h-auto hover:scale-[1.02] transition-all duration-200 ease-in-out'>
-                    <div className='absolute bottom-5 -left-1 p-2 pl-3 bg-[#84D2F6] group-hover:bg-[#59A5D8] transition-colors duration-200 ease-in-out rounded-r-lg max-w-[250px]'>
-                        <h2 className='font-bold text-white text-lg'>Project 4</h2>
+                <a href="http://example.com/" target="_blank" rel="noopener noreferrer"
+                className='group relative flex h-[300px] w-[100%] md:w-[48%] transition-all duration-300 ease-in-out
+                rounded-xl drop-shadow-md hover:drop-shadow-2xl border-8 border-[#84D2F6] hover:border-[#59A5D8] hover:scale-[1.02]
+                bg-center bg-cover bg-no-repeat backdrop-blur-none hover:backdrop-blur-sm' style={{ backgroundImage: `url(${pic})` }}>
+                    <div className='absolute bottom-5 -left-1 p-2 pl-3 bg-[#84D2F6] group-hover:bg-[#59A5D8] transition-colors duration-300 ease-in-out rounded-r-lg max-w-[250px]'>
+                        <h2 className='font-bold text-white text-lg'>Project 1</h2>
                         <h3 className='text-sm text-[#133C55]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, sint?</h3>
                     </div>
-                </div>
+                    <a href="https://github.com/csutrick" className='absolute bottom-0 right-0 p-2 '>
+                        <AiFillGithub className='text-5xl hover:scale-[115%] text-[#84D2F6] group-hover:text-[#6bce70] transition-all duration-300 ease-in-out'/>
+                    </a>
+                </a>
                 {/* Project 5 */}
-                <div className='bg-gray-200 p-4 col-start-3 col-end-7 relative drop-shadow-md hover:drop-shadow-2xl
-                group rounded-xl border-[#84D2F6] hover:border-[#59A5D8] border-8 h-[300px] hover:scale-[1.02] transition-all duration-200 ease-in-out'>
-                    <div className='absolute bottom-5 -left-1 p-2 pl-3 bg-[#84D2F6] group-hover:bg-[#59A5D8] transition-colors duration-200 ease-in-out rounded-r-lg max-w-[250px]'>
-                        <h2 className='font-bold text-white text-lg'>Project 5</h2>
+                <a href="http://example.com/" target="_blank" rel="noopener noreferrer"
+                className='group relative flex h-[300px] w-[100%] md:w-[48%] transition-all duration-300 ease-in-out
+                rounded-xl drop-shadow-md hover:drop-shadow-2xl border-8 border-[#84D2F6] hover:border-[#59A5D8] hover:scale-[1.02]
+                bg-center bg-cover bg-no-repeat backdrop-blur-none hover:backdrop-blur-sm' style={{ backgroundImage: `url(${pic})` }}>
+                    <div className='absolute bottom-5 -left-1 p-2 pl-3 bg-[#84D2F6] group-hover:bg-[#59A5D8] transition-colors duration-300 ease-in-out rounded-r-lg max-w-[250px]'>
+                        <h2 className='font-bold text-white text-lg'>Project 1</h2>
                         <h3 className='text-sm text-[#133C55]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, sint?</h3>
                     </div>
-                </div>
+                    <a href="https://github.com/csutrick" className='absolute bottom-0 right-0 p-2 '>
+                        <AiFillGithub className='text-5xl hover:scale-[115%] text-[#84D2F6] group-hover:text-[#6bce70] transition-all duration-300 ease-in-out'/>
+                    </a>
+                </a>
             </div>
         </section>
     )
