88caf6fd353b1e8c1f1886945fda5272e8eb7180
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2024-01-19 12:00:18 -0600

    Added urls to projects,, touched up styles

--- src/components/Language/index.js
+++ src/components/Language/index.js
@@ -10,7 +10,7 @@ const Language = () => {
         <section id="language" 
         className='w-full flex flex-col justify-center items-center p-4 
         bg-[#84D2F6] border-8 border-[#59A5D8] rounded-xl drop-shadow-md'>
-            <h2 className="px-8 pb-2 mb-2 text-white font-bold text-6xl 
+            <h2 className="px-6 sm:px-8 pb-2 mb-2 text-white font-bold text-5xl sm:text-6xl 
             whitespace-nowrap border-b-4 border-white drop-shadow-md">
                 Languages
             </h2>
